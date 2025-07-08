5a803ebcfb38de47cf2a3f50a5759bb980c98d82
Daniel Nunez <danielnunez@avantiway.com>
2025-07-08 00:24:19 -0400

    Add bootstrap module to register FastAPI controllers dynamically

---
+++ core/bootstrap.py
@@ -0,0 +1,14 @@
+import importlib
+import pkgutil
+import controllers
+from fastapi import FastAPI
+
+def register_controllers(app: FastAPI) -> None:
+    modules = pkgutil.iter_modules(controllers.__path__)
+    for _, module_name, _ in modules:
+        module = importlib.import_module(f"controllers.{module_name}")
+        if hasattr(module, "router"):
+            app.include_router(module.router)
+
+def run(app: FastAPI) -> None:
+    register_controllers(app)
